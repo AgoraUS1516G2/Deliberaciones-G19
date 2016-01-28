4e45b1032b595a316a1bb0b345de7b23f8fc4f32
espcrusue <espcrusue@alum.us.es>
2016-01-28 18:50:54 +0100

    Depuracon de la clase Hilo.java

--- src/main/java/domain/Hilo.java
+++ src/main/java/domain/Hilo.java
@@ -1,4 +1,3 @@
-
 package domain;
 import java.util.Collection;
 import java.util.Date;
@@ -15,23 +14,32 @@ import javax.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.SafeHtml;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.test.annotation.Timed;
+
 @Entity
 @Access(AccessType.PROPERTY)
 public class Hilo extends DomainEntity{
 	
-	
-	
+	//Atributtes
 	private String title;
 	private Date creationMoment;
 	private String text;
 	
+	//Constructor
+	public Hilo(){
+		super();
+	}
 	
+	//RelationShips
+	private User user;
+	private Collection<Comment> comments;
+	
+	//Getters and Setters
 	@NotBlank
 	@SafeHtml
 	public String getTitle() {
 		return title;
 	}
+	
 	public void setTitle(String title) {
 		this.title = title;
 	}
@@ -42,6 +50,7 @@ public class Hilo extends DomainEntity{
 	public Date getCreationMoment() {
 		return creationMoment;
 	}
+	
 	public void setCreationMoment(Date creationMoment) {
 		this.creationMoment = creationMoment;
 	}
@@ -51,32 +60,28 @@ public class Hilo extends DomainEntity{
 	public String getText() {
 		return text;
 	}
+	
 	public void setText(String text) {
 		this.text = text;
 	}
-	
-	//relationShips
-	
-	private User user;
-	private Collection<Comment> comments;
 	
 	@NotNull
 	@ManyToOne(optional=false)
 	public User getUser() {
 		return user;
 	}
+	
 	public void setUser(User user) {
 		this.user = user;
 	}
+	
 	@NotNull
 	@OneToMany(mappedBy="thread")
 	public Collection<Comment> getComments() {
 		return comments;
 	}
+	
 	public void setComments(Collection<Comment> comments) {
 		this.comments = comments;
 	}
-	
-	
-	
 }
