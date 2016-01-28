4834c2a8ee4da8270ec8f03610ae1a60133ccead
espcrusue <espcrusue@alum.us.es>
2016-01-28 18:43:49 +0100

    Depuracion de la clase Administrator.java

--- src/main/java/domain/Administrator.java
+++ src/main/java/domain/Administrator.java
@@ -1,11 +1,16 @@
 
 package domain;
-import java.util.Collection;
 
 import javax.persistence.Access;
 import javax.persistence.AccessType;
 import javax.persistence.Entity;
+
 @Entity
 @Access(AccessType.PROPERTY)
 public class Administrator extends Actor{
+	
+	//Constructor
+	public Administrator(){
+		super();
+	}
 }
