ef7ea7eb997b868d4a116211233be4dccc9587ff
espcrusue <esperanzita_93@hotmail.com>
2016-02-02 15:46:49 +0100

    Añadir nueva busqueda de hilos dado un usuario, aunque aun no esta implementada correctamente

--- src/main/java/services/ThreadService.java
+++ src/main/java/services/ThreadService.java
@@ -1,23 +1,35 @@
-
 package services;
+
 import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import repositories.ThreadRepository;
 import domain.Hilo;
+
 @Service
 @Transactional
 public class ThreadService {
-@Autowired
+
+	// Managed repository --------------------
+	@Autowired
 	private ThreadRepository threadRepository;
-public Collection<Hilo>  findAll(){
-return threadRepository.findAll();
-}
-public Hilo findOne(Integer valueOf) {
-return threadRepository.findOne(valueOf);
-}
-public Hilo save(Hilo thread){
-return threadRepository.save(thread);
-}
+
+	// Simple CRUD methods ----------
+	public Hilo save(Hilo thread){
+		return threadRepository.save(thread);
+	}
+	
+	public Collection<Hilo>  findAll(){
+		return threadRepository.findAll();
+	}
+	
+	public Hilo findOne(int threadId) {
+		return threadRepository.findOne(threadId);
+	}
+	
+	//Other business methods -------------------
+	public Hilo findThreadByComment(int commentId) {
+		return threadRepository.findThreadByComment(commentId);
+	}
 }
