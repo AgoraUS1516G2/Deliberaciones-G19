b25bf952c01c183609a3af39b730d700e72f7000
Pablo <pab993@hotmail.com>
2016-02-03 23:09:42 +0100

    Pequeña modificación en userService

--- src/main/java/services/UserService.java
+++ src/main/java/services/UserService.java
@@ -5,24 +5,24 @@ import java.util.Collection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
 
 import repositories.UserRepository;
 import security.LoginService;
+import security.UserAccountRepository;
+import domain.Ban;
 import domain.User;
 
 @Service
 @Transactional
 public class UserService {
-	
+
 	// Managed repository --------------------
 	@Autowired
 	private UserRepository userRepository;
 	
-	
-	// Simple CRUD methods ----------
-	public User save(User user){
-		return userRepository.save(user);
-	}
+	@Autowired
+	private BanService banService;
 	
 	public Collection<User>  findAll(){
 		return userRepository.findAll();
@@ -32,12 +32,37 @@ public class UserService {
 		return userRepository.findOne(valueOf);
 	}
 	
+
 	//Other business methods -------------------
+
+	public User create(){
+		User result;
+		Ban res;
+		result = new User();
+		res = banService.create();
+		result.setBan(res);
+		return result;
+	}
+	
+	public User save(User user){
+		Assert.notNull(user);	
+		return userRepository.save(user);
+	}
+	
+	public void delete(User user){
+		Assert.notNull(user);
+		banService.delete(user.getBan());
+		userRepository.delete(user);
+	}
+	
 	public User findByPrincipal() {
+	// TODO Auto-generated method stub
 		return userRepository.findOneByPrincipal(LoginService.getPrincipal().getId());
 	}
 	
 	public User findByUsername(String username) {
+	// TODO Auto-generated method stub
 		return userRepository.findByUsername(username);
 	}
+	
 }
