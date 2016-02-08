1f856af8b2cc1c04c73c5204e9bb2a39e797ff73
MJ Palma <mjpalmaguerrero@gmail.com>
2016-02-08 12:49:11 +0100

    Cambios controlador
    Cambios realizados en el controlador del customer comment

--- src/main/java/controllers/CustomerCommentController.java
+++ src/main/java/controllers/CustomerCommentController.java
@@ -13,130 +13,127 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import domain.Comment;
-import domain.User;
-
-import security.LoginService;
 import services.ActorService;
 import services.CommentService;
 import services.ThreadService;
 import services.UserService;
+import domain.Comment;
+import domain.User;
 
 @Controller
 @RequestMapping("/comment/customer")
-public class CustomerCommentController extends AbstractController{
+public class CustomerCommentController extends AbstractController {
 
 	// Services ---------------------------------------------------------------
-	@Autowired 
+	@Autowired
 	CommentService commentService;
-	
-	@Autowired 
+
+	@Autowired
 	ActorService actorService;
-	
-	@Autowired 
+
+	@Autowired
 	ThreadService threadService;
-	
-	@Autowired 
+
+	@Autowired
 	UserService userService;
-		
+
 	// Constructors -----------------------------------------------------------
 	public CustomerCommentController() {
 		super();
 	}
-	
 
 	// Listing ----------------------------------------------------------------
-	@RequestMapping(value="/list", method= RequestMethod.GET)
-    public ModelAndView list(){
-        ModelAndView result;
-        
-        Collection<Comment> comments;
-        User user;
-        user = userService.findByPrincipal();
-        comments = commentService.findCommentByUser(user);
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public ModelAndView list() {
+		ModelAndView result;
 
-        result= new ModelAndView("comment/customer/list");
-        result.addObject("comments", comments);
-        result.addObject("requestURI", "/comment/customer/list.do");
-         
-        return result;
-    }
-	
+		Collection<Comment> comments;
+		User user;
+		user = userService.findByPrincipal();
+		comments = commentService.findCommentByUser(user);
+
+		result = new ModelAndView("comment/customer/list");
+		result.addObject("comments", comments);
+		result.addObject("requestURI", "/comment/customer/list.do");
+
+		return result;
+	}
+
 	// Creation ---------------------------------------------------------------
 	@RequestMapping(value = "/create", method = RequestMethod.GET)
 	public ModelAndView create() {
 		ModelAndView result;
 		Comment comment;
-		
-		comment = commentService.create();		
+
+		comment = commentService.create();
 		result = createEditModelAndView(comment);
-		
+
 		return result;
 	}
 
-	
 	// Edition ----------------------------------------------------------------
-	@RequestMapping(value="/edit", method=RequestMethod.GET)
-    public ModelAndView edit(@RequestParam int commentId){
-        ModelAndView result;
-        Comment comment;
-        User user;
-        
-        user= userService.findByPrincipal();
-        comment = commentService.findOne(commentId);
-        Assert.isTrue(comment.getUser().equals(user));
-        result= createEditModelAndView(comment);
-          
-        return result;
-    }
-      
-    @RequestMapping(value="/edit", method=RequestMethod.POST, params="save")
-    public ModelAndView save(@Valid Comment comment, BindingResult binding){
-        ModelAndView result;
-          
-        if(binding.hasErrors()){
-            result= createEditModelAndView(comment);
-        }else{
-            try{
-                commentService.save(comment);
-                result= new ModelAndView("redirect:list.do");
-            }catch(Throwable oops){
-                result= createEditModelAndView(comment, "comment.commit.error");
-            }
-        }
-        return result;
-    }
-      
-    @RequestMapping(value="/edit", method=RequestMethod.POST, params = "delete")
-    public ModelAndView delete(Comment comment){
-        ModelAndView result;
-        try {			
+	@RequestMapping(value = "/edit", method = RequestMethod.GET)
+	public ModelAndView edit(@RequestParam int commentId) {
+		ModelAndView result;
+		Comment comment;
+		User user;
+
+		user = userService.findByPrincipal();
+		comment = commentService.findOne(commentId);
+		Assert.isTrue(comment.getUser().equals(user));
+		result = createEditModelAndView(comment);
+
+		return result;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "save")
+	public ModelAndView save(@Valid Comment comment, BindingResult binding) {
+		ModelAndView result;
+
+		if (binding.hasErrors()) {
+			result = createEditModelAndView(comment);
+		} else {
+			try {
+				commentService.save(comment);
+				result = new ModelAndView("redirect:list.do");
+			} catch (Throwable oops) {
+				result = createEditModelAndView(comment, "comment.commit.error");
+			}
+		}
+		return result;
+	}
+
+	@RequestMapping(value = "/edit", method = RequestMethod.POST, params = "delete")
+	public ModelAndView delete(Comment comment) {
+		ModelAndView result;
+		try {
 			commentService.delete(comment);
 			result = new ModelAndView("redirect:list.do");
 		} catch (Throwable oops) {
-			result = createEditModelAndView(comment, "comment.commit.error");			
+			result = createEditModelAndView(comment, "comment.commit.error");
 		}
-        return result;
-          
-    }
-     
-    // Ancillary methods ------------------------------------------------
-    protected ModelAndView createEditModelAndView(Comment comment){
-        ModelAndView result;
-          
-        result= createEditModelAndView(comment, null);
-          
-        return result;
-    }
-      
-    protected ModelAndView createEditModelAndView(Comment comment, String message){
- 
-        ModelAndView result;
-          
-        result= new ModelAndView("comment/customer/edit");
-        result.addObject("comment", comment);
-        result.addObject("message", message);
-         
-        return result;
-    }
+		return result;
+
+	}
+
+	// Ancillary methods ------------------------------------------------
+	protected ModelAndView createEditModelAndView(Comment comment) {
+		ModelAndView result;
+
+		result = createEditModelAndView(comment, null);
+
+		return result;
+	}
+
+	protected ModelAndView createEditModelAndView(Comment comment,
+			String message) {
+
+		ModelAndView result;
+
+		result = new ModelAndView("comment/customer/edit");
+		result.addObject("comment", comment);
+		result.addObject("message", message);
+
+		return result;
+	}
 }
